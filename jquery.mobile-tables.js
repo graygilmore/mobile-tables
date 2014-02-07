547030943431718277da265b633f78e3a7b5290a
Gray Gilmore <graygilmore@gmail.com>
2014-02-06 22:35:38 -0800

    Add styles via js

--- jquery.mobile-tables.js
+++ jquery.mobile-tables.js
@@ -18,6 +18,27 @@
           $.extend( settings, options );
         }
 
+        var applyStyles = function() {
+            var tableStyles = ".column-title { font-weight: 700; width: 114px; padding-right: 23px; display: none; }";
+            tableStyles += " .mobile-layout .column-title { display: -moz-inline-stack; display: inline-block; vertical-align: middle; zoom: 1; }";
+            tableStyles += " .mobile-layout thead { display: none; }";
+            tableStyles += " .mobile-layout tr td { width: 100%; display: block; text-align: left; }";
+            tableStyles += " .mobile-layout tr:nth-child(even) td { background: #ddd; }";
+
+            head = document.getElementsByTagName('head')[0],
+            style = document.createElement('style');
+
+            style.type = 'text/css';
+            if (style.styleSheet){
+                style.styleSheet.cssText = tableStyles;
+            } else {
+                style.appendChild(document.createTextNode(tableStyles));
+            }
+
+            head.appendChild(style);
+        };
+        if( !$("style:contains('.mobile-layout')").length ) { applyStyles(); }
+
         return this.each( function () {
 
             var table = $(this);
